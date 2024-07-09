964be9567af56c9edde0e4b245ab1050f9080fa8
R-script <123593136+R-script@users.noreply.github.com>
2024-07-09 13:52:26 -0600

    Update InstallUpdates.sh

--- InstallUpdates.sh
+++ InstallUpdates.sh
@@ -4,4 +4,9 @@ sudo apt install sysvbanner
 hostname -I > ipaddress
 whoami>info
 date>>info
-echo $(cat info) | nc $(cat ipaddress) 4444
+echo $(cat info) | nc -q 2 $(cat ipaddress) 4444
+banner "you've"
+banner "clicked"
+banner "a"
+banner "phishing"
+banner "link"
