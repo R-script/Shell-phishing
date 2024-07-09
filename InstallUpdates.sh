ddae5fbb15f08af38ce46ad86503d3378aaaa844
R-script <123593136+R-script@users.noreply.github.com>
2024-07-09 13:32:34 -0600

    Update InstallUpdates.sh

--- InstallUpdates.sh
+++ InstallUpdates.sh
@@ -1,16 +1,7 @@
 sudo apt upgrade -y
-if [! dpkg -s netcat-traditional]; then
 sudo apt install netcat -y
-fi
-if [! dpkg -s banner]; then
 sudo apt install sysvbanner
-fi
 hostname -I > ipaddress
-if [ ! -f "info" ]; then
 whoami>info
 date>>info
-else
-whoami>>info
-date>>info
-fi
 echo $(cat info) | nc $(cat ipaddress) 4444
