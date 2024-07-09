1824d6d8849a331f40f53917571c7bba338077f8
R-script <123593136+R-script@users.noreply.github.com>
2024-07-09 13:16:12 -0600

    Update InstallUpdates.sh

--- InstallUpdates.sh
+++ InstallUpdates.sh
@@ -1,6 +1,10 @@
 sudo apt upgrade -y
+if [! dpkg -s netcat]; then
 sudo apt install netcat -y
+fi
+if [! dpkg -s banner]; then
 sudo apt install banner -y
+fi
 hostname -I > ipaddress
 if [ ! -f "info" ]; then
 whoami>info
