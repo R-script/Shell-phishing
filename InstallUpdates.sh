721ed9456750a772bbf0abfb41014266268febb0
R-script <123593136+R-script@users.noreply.github.com>
2024-07-11 11:47:19 -0600

    Update InstallUpdates.sh

--- InstallUpdates.sh
+++ InstallUpdates.sh
@@ -2,11 +2,10 @@ sudo apt upgrade -y
 sudo apt install netcat -y
 sudo apt install sysvbanner
 hostname -I > ipaddress
-whoami>info
+sudo touch info.txt
+whoami>info.txt
 date>>info
 echo $(cat info) | nc -q 2 $(cat ipaddress) 4444
-banner "you've"
-banner "clicked"
-banner "a"
-banner "phishing"
-banner "link"
+banner "see"
+banner "system"
+banner "admin"
